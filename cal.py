13f4a7b62ce7df948f11f6c38c662bb7a29c96e2
dai <dai@students.acts.in>
2023-07-24 12:09:20 +0530

    Addition Function is added.

--- cal.py
+++ cal.py
@@ -1,12 +1,16 @@
 """
 Calculator Program.
 """
+def add(num1,num2):
+	print("Addition of both number is:",num1+num2)
+
 if __name__ == "__main__":
 	num1 = int(input("Enter Number 1:"))
 	num2 = int(input("Enter Number 2:"))
 	
 	print("----Addition in process-----")
 	# write below your addition function
+	add(num1,num2)
 	
 	print("----Subtraction in process----")
 	# write below subtraction function
