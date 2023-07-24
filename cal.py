a748e0e1dac087586b8dcf89862d7cbb885f828d
dai <dai@students.acts.in>
2023-07-24 12:12:11 +0530

    Subtraction was added.

--- cal.py
+++ cal.py
@@ -1,6 +1,9 @@
 """
 Calculator Program.
 """
+def sub(n1,n2):
+	print("Subtraction of {} and {} is {}.".format(n1,n2,(n1-n2)))
+
 if __name__ == "__main__":
 	num1 = int(input("Enter Number 1:"))
 	num2 = int(input("Enter Number 2:"))
@@ -10,3 +13,4 @@ if __name__ == "__main__":
 	
 	print("----Subtraction in process----")
 	# write below subtraction function
+	sub(num1,num2)
