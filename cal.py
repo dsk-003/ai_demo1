408ebfd87d9e897412cc9e0a2e5b618e97774978
dai <dai@students.acts.in>
2023-07-24 11:52:50 +0530

    Before branching.

--- cal.py
+++ cal.py
@@ -1,3 +1,12 @@
 """
 Calculator Program.
 """
+if __name__ == "__main__":
+	num1 = int(input("Enter Number 1:"))
+	num2 = int(input("Enter Number 2:"))
+	
+	print("----Addition in process-----")
+	# write below your addition function
+	
+	print("----Subtraction in process----")
+	# write below subtraction function
